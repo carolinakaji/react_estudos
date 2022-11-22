1707dcd7919ea71521a08fcc4c43fba9ede82c45
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-22 15:02:46 -0300

    Comentários

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -5,8 +5,10 @@ import FirstComponent from './components/FirstComponent'
 import './App.css';
 
 function App() {
+  // Testando os comentários
   return (
     <div className="App">
+      {/* Comentário no JSX */}
       <h1>Fundamentos</h1>
       <h2>Criando componente</h2>
       <FirstComponent />
