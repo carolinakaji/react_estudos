210e36ea41cd20acac1930adfe3f7da5f051ee5f
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-22 16:47:27 -0300

    MyComponent na hierarquia de filho do FirstComponent

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,9 +1,12 @@
+import MyComponent from "./MyComponent";
+
 const FirstComponent = () => {
 
   return (
 
     <div>
       <p className="texto-verde">Meu primeiro componente</p>
+      <MyComponent />
       <p>teste</p>
     </div>
   );
