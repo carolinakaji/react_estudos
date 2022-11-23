926f3f42a6a7eae67c853c7e6a9b5a28847dda01
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-23 11:42:40 -0300

    Incluindo imagens - public folder e assets folder

--- 2_FUNDAMENTOS/desafio3/src/App.js
+++ 2_FUNDAMENTOS/desafio3/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Img2 from './assets/img2.jpg'
 
 function App() {
   return (
     <div className="App">
       <h1>Avançando no React</h1>
+      <div>
+        {/*When the img is inside the public folder, it's not necessary to import the img. It's linked to the src attribute*/}
+        <h2>Imagens no public</h2>
+        <img className="img_gram" src="/img1.jpg" alt="Ilha vista do alto" />
+      </div>
+      <div>
+        {/*When the img is inside the src/assets folder, it's necessary to import the img and call it as a template expression in src*/}
+        <h2>Imagens no assets</h2>
+        <img className="img_gram" src={Img2} alt="Ilha vista do alto" />
+      </div>
     </div>
   );
 }
