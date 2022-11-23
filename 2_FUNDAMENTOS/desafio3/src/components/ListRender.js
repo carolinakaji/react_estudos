a5878211f00889635ec5f5000decd166d239ded2
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-23 14:48:55 -0300

    Usando a propriedade key com o id do produto

--- 2_FUNDAMENTOS/desafio3/src/components/ListRender.js
+++ 2_FUNDAMENTOS/desafio3/src/components/ListRender.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
 
 const ListRender = () => {
-  const [list] = useState(['Arroz', 'Alface', 'Lentilha', 'Tomate', 'Amendoim'])
+  const [list] = useState(['Arroz', 'Alface', 'Lentilha', 'Tomate', 'Amendoim']);
+
+  const [products] = useState([
+    {id: 123, product: 'Rice', price: 18.00},
+    {id: 124, product: 'Letuce', price: 3.00},
+    {id: 125, product: 'Tomato', price: 5.00},
+    {id: 126, product: 'Peanut', price: 15.00},
+    {id: 127, product: 'Cheese', price: 7.00},
+  ])
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {/* using the index of an array is not recommended, because it can change.  */}
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
         ))}
       </ul>
+
+      {/* The best way to use a key is associating it to the id that comes from database.  */}
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>{product.product} - U${product.price}</li>
+            ))}
+          </ul>
     </div>
   )
 }
