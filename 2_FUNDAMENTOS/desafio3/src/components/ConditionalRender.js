05e9529b0089ecda17d06c102b2b0c1b02f4aa7f
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-24 13:45:12 -0300

    Renderização condicioal com ternário

--- 2_FUNDAMENTOS/desafio3/src/components/ConditionalRender.js
+++ 2_FUNDAMENTOS/desafio3/src/components/ConditionalRender.js
@@ -1,14 +1,25 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const ConditionalRender = () => {
 
   const [x] = useState(false);
+  const [nome, setNome] = useState("Joana")
+
   return (
     <div>
-      <h1>Exibe ou não?</h1>
+      <h2>Exibe ou não?</h2>
       {x && <p>Exibe quando x for true</p>}
       {!x && <p>Exibe quando x for not false</p>}
+      <h2>If ternário</h2>
+      {nome === "Joana"
+        ? <div><p>Nome Joana</p></div>
+        : <div><p>Nao existe {nome}</p></div>}
+      <button onClick={() => {
+        setNome('Malu');
+        console.log(nome);
+      }}>Alterar Nome</button>
     </div>
+
   );
 };
 
