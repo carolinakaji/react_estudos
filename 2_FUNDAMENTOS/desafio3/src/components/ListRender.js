41bfdc0b5c1acb18861cd754934c371f925e01c4
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-24 08:36:02 -0300

    previous state

--- 2_FUNDAMENTOS/desafio3/src/components/ListRender.js
+++ 2_FUNDAMENTOS/desafio3/src/components/ListRender.js
@@ -3,13 +3,23 @@ import React, { useState } from 'react';
 const ListRender = () => {
   const [list] = useState(['Arroz', 'Alface', 'Lentilha', 'Tomate', 'Amendoim']);
 
-  const [products] = useState([
-    {id: 123, product: 'Rice', price: 18.00},
-    {id: 124, product: 'Letuce', price: 3.00},
-    {id: 125, product: 'Tomato', price: 5.00},
-    {id: 126, product: 'Peanut', price: 15.00},
-    {id: 127, product: 'Cheese', price: 7.00},
+  const [products, setProducts] = useState([
+    { id: 1, product: 'Rice', price: 18.00 },
+    { id: 2, product: 'Letuce', price: 3.00 },
+    { id: 3, product: 'Tomato', price: 5.00 },
+    { id: 4, product: 'Peanut', price: 15.00 },
+    { id: 5, product: 'Cheese', price: 7.00 },
   ])
+
+  const deleteRandom = () => {
+    const randomNumber = Math.ceil(Math.random() * 5);
+    console.log(randomNumber);
+    setProducts((prevProducts) => {
+      // filtra a array e devolve os produtos cujo id são diferentes do número gerado.
+      return prevProducts.filter((product) => randomNumber !== product.id)
+    })
+  }
+
   return (
     <div>
       <ul>
@@ -20,12 +30,15 @@ const ListRender = () => {
       </ul>
 
       {/* The best way to use a key is associating it to the id that comes from database.  */}
-          <ul>
-            {/* Pushing a new product {products.push({id: 128, product: 'Sofa', price: 800.00})}*/}
-            {products.map((product) => (
-              <li key={product.id}>{product.product} - U${product.price}</li>
-            ))}
-          </ul>
+      <ul>
+        {/* Pushing a new product {products.push({id: 128, product: 'Sofa', price: 800.00})}*/}
+        {products.map((product) => (
+          <li key={product.id}>{product.product} - U${product.price}</li>
+        ))}
+      </ul>
+      <div>
+        <button onClick={deleteRandom}>Delete random user</button>
+      </div>
     </div>
   )
 }
