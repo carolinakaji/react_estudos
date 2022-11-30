9a722ef71500af73d0d8768923a92686bfe3360c
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-30 17:22:05 -0300

    label envolvendo input

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -9,6 +9,13 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite seu nome"/>
         </div>
+        {/* 2 - label envolvendo input - doc indica esta abordagem*/}
+        <div>
+          <label>
+            <span>E-mail:</span>
+            <input type="email" name="email" placeholder="Digite o seu e-mail" />
+          </label>
+        </div>
         <input type="submit" value="Enviar" />
       </form>
     </div>
