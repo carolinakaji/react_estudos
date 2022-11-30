2a0cc26d4b756a454dc67842fd8c458040b40dc1
Carolina Kaji <carolina.kaji@gmail.com>
2022-11-30 14:03:56 -0300

    CSS inline

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -12,6 +12,11 @@ function App() {
       <p>Este é o parágrafo do 
         App.js
       </p>
+
+      {/* Inline CSS - não é muito aplicado pois dificulta a manutenabilidade */}
+      <p style={{color: "blue", padding:"25px", borderTop:"2px solid red", backgroundColor: "#fff"}}>Este elemento foi estilizado de forma inline</p>
+      <p style={{color: "magenta", padding:"25px", borderTop:"2px solid red", backgroundColor: "#fff"}}>Este elemento foi estilizado de forma inline</p>
+
     </div>
   );
 }
